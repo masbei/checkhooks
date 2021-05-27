e219ff7b5abd2558a27d70934e74f245ef478db6
masbei <mohamedamine.sbei@esprit.tn>
2021-05-27 16:22:43 +0200

    final

--- src/components/Description.js
+++ src/components/Description.js
@@ -8,6 +8,7 @@ const Description = ({match}) => {
         <div>
             <iframe width="560" height="315" 
             src={filtredList[0].trailer} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <p>{filtredList[0].description}</p>
         </div>
     )
 }
--- src/moviesData.js
+++ src/moviesData.js
@@ -6,7 +6,7 @@ duration:"106 Min",
 year:"2021",
 rating:"4",
 image:'https://media.senscritique.com/media/000019951246/source_big/Thunder_Force.jpg',
-description:"",
+description:"In a world where supervillains are commonplace, two estranged childhood best friends reunite after one devises a treatment that gives them powers to protect their city.",
 trailer:"https://www.youtube.com/embed/qnx6-YLXFwg",
 },
 
@@ -19,8 +19,8 @@ duration:"50 Min",
 year:"2021",
 rating:"7",
 image:'https://www.ecranlarge.com/uploads/image/001/366/falcon-et-le-soldat-de-lhiver-affiche-francaise-1366175.jpg',
-description:"",
-trailer:"",   
+description:"Following the events of 'Avengers: Endgame,' Sam Wilson/Falcon and Bucky Barnes/Winter Soldier team up in a global adventure that tests their abilities -- and their patience.",
+trailer:"https://www.youtube.com/embed/sSqf6f8ZWHQ" ,  
     },
 {
 name:"Line of Duty",
@@ -29,8 +29,8 @@ duration:"60 Min",
 year:"2012",
 rating:"8",
 image:'https://m.media-amazon.com/images/M/MV5BMTExZDZjNTMtNDVmNy00ZTk2LWFiMzUtZDlkZGRlOGU0ZWRmXkEyXkFqcGdeQXVyMjExMjk0ODk@._V1_.jpg',
-description:"",
-trailer:"",       
+description:"In a bid to uncover the identity of 'H', AC-12 interview murder suspect DC Taylor. With no help from his ineffective lawyer.",
+trailer:"https://www.youtube.com/embed/LbKIzP4bmFA",       
         },    
 
 {
@@ -40,8 +40,8 @@ duration:"24 Min",
 year:"2013",
 rating:"9",           
 image:'https://fr.web.img6.acsta.net/pictures/20/12/28/10/24/5603983.jpg',                       
-description:"",
-trailer:"",           
+description:"After his hometown is destroyed and his mother is killed, young Eren Jaeger vows to cleanse the earth of the giant humanoid Titans that have brought humanity to the brink of extinction",
+trailer:"https://www.youtube.com/embed/ymDAiSQFIrg",           
 },
 
 {
@@ -51,6 +51,8 @@ duration:"130 Min",
 year:"2011",                
 rating:"8",                
 image:'https://media.senscritique.com/media/000004753305/source_big/Harry_Potter_et_les_Reliques_de_la_Mort_2eme_partie.jpg',
+description:"Harry, Ron, and Hermione search for Voldemort's remaining Horcruxes in their effort to destroy the Dark Lord as the final battle rages on at Hogwarts.",
+trailer:"https://www.youtube.com/embed/aiaIfICU-Tk"
  },
 
 {    
@@ -60,7 +62,7 @@ duration:"162 Min",
 year:"2001",                    
 rating:"7",                    
 image:'https://fr.web.img2.acsta.net/pictures/18/07/02/17/25/3643090.jpg',  
-description:"",
+description:"An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world.",
 trailer:"https://www.youtube.com/embed/ht5T2thYQFk",     }
 
 
